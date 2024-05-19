c3d74c5c2c99953b595ea772db67fa30b93111a9
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-05-19 12:06:04 +0530

    Chat detail component

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,61 @@ import './detail.css';
 
 const Detail = () =>{
     return(
-        <div className="detail">Detail</div>
+        <div className="detail">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                    <div className="option">
+                    <div className="title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                 
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://images.pexels.com/photos/2412720/pexels-photo-2412720.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                            <span>photo_2024_2.png</span>
+
+                            </div>
+                            
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://images.pexels.com/photos/2412720/pexels-photo-2412720.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                            <span>photo_2024_2.png</span>
+
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+               
+                <div className="option">
+                    <div className="title">
+                        <span>Shared files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
