ae3740a3959ae7b3f98598a3112b34068577c0f2
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-05-21 22:38:08 +0530

    React Firebase How to Upload Image?

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,13 +5,16 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../lib/firebase';
 import { doc , setDoc } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
+import upload from '../../lib/upload';
 
 const Login = () =>{
 
     const [avatar,setAvatar] = useState({
         file : null,
         url :"",
-    })
+    });
+
+    const [loading, setLoading] = useState(false)
 
     const handleAvatar = e =>{
         if(e.target.files[0]){
@@ -24,17 +27,21 @@ const Login = () =>{
 
 const handleRegister = async e => {
     e.preventDefault();
+    setLoading(true)
     const formData = new FormData(e.target);
 
     const {username,email,password} = Object.fromEntries(formData);
 
     try {
 
-        const res = await createUserWithEmailAndPassword(auth,email,password)
+        const res = await createUserWithEmailAndPassword(auth,email,password);
+
+        const imgUrl = await upload(avatar.file)
 
         await setDoc(doc(db, "users", res.user.uid), {
             username,
             email,
+            avatar: imgUrl,
             id : res.user.uid,
             blocked : [],
             
@@ -50,6 +57,8 @@ const handleRegister = async e => {
     } catch (error) {
         console.log(error)
         toast.error(error.message)
+    }finally{
+        setLoading(false)
     }
     
 };
@@ -70,7 +79,7 @@ const handleLogin = e => {
                 <form onSubmit={handleLogin} >
                     <input type="text"  placeholder='Email' name="email" />
                     <input type="password"  placeholder='Password' name="password" />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{loading ? "Loading" :"Sign In"}</button>
                 </form>
             </div>
             <div className="separator"></div>
@@ -86,7 +95,7 @@ const handleLogin = e => {
                     <input type="text"  placeholder='Username' name="username" />
                     <input type="text"  placeholder='Email' name="email" />
                     <input type="password"  placeholder='Password' name="password" />
-                    <button>Sign Up</button>
+                    <button disabled={loading}>{loading ? "Loading" :"Sign Up"}</button>
                 </form>
             </div>
             
